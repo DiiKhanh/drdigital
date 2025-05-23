056f22bf4dd0c62125cf2b9748fd9d3a0bf5b7de
DiiKhanh <duykhanh.030803@gmail.com>
2025-05-23 15:26:07 +0700

    feat: change ui app sidebar

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -10,14 +10,17 @@ import { NavGroup } from '@/components/layout/nav-group'
 // import { TeamSwitcher } from '@/components/layout/team-switcher'
 import { sidebarData } from './data/sidebar-data'
 import logo from '/images/logo.svg'
-import { IconLogout } from '@tabler/icons-react'
+import { IconHelp, IconLogout, IconSettings, IconUser } from '@tabler/icons-react'
 import { Button } from '../ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu'
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible='icon' variant='floating' {...props}>
       <SidebarHeader className='mt-10'>
-        <img src={logo} alt='logo' />
+        <a href='/'>
+          <img src={logo} alt='logo' />
+        </a>
       </SidebarHeader>
       <SidebarContent className='mt-20'>
         {sidebarData.navGroups.map((props) => (
@@ -25,13 +28,47 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         ))}
       </SidebarContent>
       <SidebarFooter>
-        <div className='flex items-center gap-2 flex-col'>
-          <img src={'/images/user.png'} alt='user' className='rounded-full' />
-          <Button variant='ghost' size='icon' className='rounded-full'>
-            <IconLogout />
-          </Button>
-        </div>
-      </SidebarFooter>
+      <div className="flex items-center justify-center p-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex items-center gap-2 flex-col cursor-pointer">
+              <div className="relative">
+                <img
+                  src={"/images/user.png"}
+                  alt="User profile"
+                  className="rounded-full w-10 h-10 object-cover border border-border hover:ring-2 hover:ring-primary/20 transition-all"
+                />
+              </div>
+              <Button variant="ghost" size="icon" className="rounded-full h-8 w-8">
+                <IconLogout className="h-4 w-4" />
+                <span className="sr-only">Logout</span>
+              </Button>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center" className="w-56">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <IconUser className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <IconSettings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <IconHelp className="mr-2 h-4 w-4" />
+              <span>Help</span>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-destructive focus:text-destructive">
+              <IconLogout className="mr-2 h-4 w-4" />
+              <span>Logout</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   )
